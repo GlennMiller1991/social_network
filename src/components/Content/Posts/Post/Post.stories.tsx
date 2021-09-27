ae4826137c39ed1c5ed7d6cd8f922fb8cf48c03d
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-09-27 15:25:40 +0300

    first story. This is post story

--- src/components/Content/Posts/Post/Post.stories.tsx
+++ src/components/Content/Posts/Post/Post.stories.tsx
@@ -1,1 +1,31 @@
 import React from "react";
+import {Post, PostType} from "./Post";
+import {v1} from "uuid";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'Post',
+    component: Post
+}
+
+const post: PostType = {
+    postText: 'Is there anybody going to listen to my story' +
+        '? All about girl who came to stay. She is a kind of girl you' +
+        'want so much, it makes you sorry, still you don\'t regret a single day',
+    postPhoto: 'https://www.culturesonar.com/wp-content/uploads/2017/08/beatles-with-a-girl-getty-600x400.jpg',
+    postDate: '07.23.2021',
+    postLikes: -1,
+    postId: v1(),
+}
+
+const changeLikesCount = (value: boolean, postId: string) => {
+    alert('from changeLikesCount with love')
+}
+
+export const DefaultPost = () => <Post postId={post.postId}
+                                       postDate={post.postDate}
+                                       postLikes={post.postLikes}
+                                       postPhoto={post.postPhoto}
+                                       postText={post.postText}
+                                       changeLikesCount={changeLikesCount}
+/>
