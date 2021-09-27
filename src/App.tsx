73766bfc05ce2e858991a94d4974eee3a0aafc8c
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-09-27 11:49:52 +0300

    New repository for social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Header} from "./components/Header/Header";
+import {Content} from "./components/Content/Content";
+import {Footer} from "./components/Footer/Footer";
+import {BrowserRouter} from "react-router-dom";
+import {stateType} from "./redux/state";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//types
+export type wayType = 0 | 1
+type AppPropsType = {
+    state: stateType
+    changeLikesCount: (value: boolean, postId: string) => void
+    changeShareStoryText: (text: string) => void
+    addPost: (text: string) => void
+}
+
+function App(props: AppPropsType) {
+    const [way, setWay] = useState<wayType>(1)
+
+    return (
+        <BrowserRouter>
+            <div id='appWrapper'>
+                {way === 0 ? <Header setWay={setWay}/> :
+                    <>
+                        <Content state={props.state}
+                                 addPost={props.addPost}
+                                 changeLikesCount={props.changeLikesCount}
+                                 changeShareStoryText={props.changeShareStoryText}/>
+                        <Footer/>
+                    </>}
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {addPost, changeLikesCount, changeShareStoryText, state, stateType} from './redux/state'
+import ReactDOM from "react-dom";
+import App from "./App";
+import {subscribe} from "./redux/state";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderEntireTree = (state: stateType) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state}
+                 changeLikesCount={changeLikesCount}
+                 addPost={addPost}
+                 changeShareStoryText={changeShareStoryText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+renderEntireTree(state);
+subscribe(renderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// tlog results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
