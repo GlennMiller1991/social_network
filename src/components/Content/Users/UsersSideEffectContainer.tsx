826352a10690dee616e8479be828d792f31136bf
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-11-07 03:03:47 +0300

    github-pages integration with rest api

--- src/components/Content/Users/UsersSideEffectContainer.tsx
+++ src/components/Content/Users/UsersSideEffectContainer.tsx
@@ -17,7 +17,13 @@ export class UsersSideEffectContainer extends React.Component<UsersPropsType> {
         document.title = 'Users Page'
         axios
             .get<responseType>(
-                `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`
+                `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`,
+                {
+                    withCredentials: true,
+                    headers: {
+                        'API-KEY': '686ffc4e-9713-4acd-8b49-1b6f4dcbd337',
+                    }
+                }
             ).then((res) => {
             this.props.setUsers(res.data.items, res.data.totalCount)
         })
