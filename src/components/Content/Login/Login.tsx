d9cd1bab378440c2d7a9d0b122c8b720e4a0fd0d
GlennMiller1991 <gatesoftommorow91@mail.ru>
2021-11-25 19:09:48 +0300

    login page

--- src/components/Content/Login/Login.tsx
+++ src/components/Content/Login/Login.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from './Login.module.css'
 
 export const Login: React.FC = React.memo(() => {
     return (
-        <div>
-            LoginPage
+        <div className={styles.main}>
+            <p className={styles.sign}>Sign in</p>
+            <form className={styles.form1}>
+                <input className={styles.un} type="text" placeholder="Username"/>
+                <input className={styles.pass} type="password" placeholder="Password"/>
+                <a className={styles.submit}>Sign in</a>
+                <p className={styles.forgot}><a href="#">Forgot Password?</a></p>
+            </form>
         </div>
     )
 })
